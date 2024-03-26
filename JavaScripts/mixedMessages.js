c91bd405c32a82bbf6f985c59f26d77e08567dcb
Tiercen <tiercen@gmail.com>
2024-03-26 15:59:25 -0400

    Updated mixedMessages.js with several arrays

--- JavaScripts/mixedMessages.js
+++ JavaScripts/mixedMessages.js
@@ -1,1 +1,7 @@
-//THis is the initial JavaScript File for the mixedMessages()
+//This is a JavaScript was created to complete the Mixed Messages assignment from CodeCademy.com
+
+//Arrays
+
+const lovingAdj = ["adorable", "beautiful", "charming", "delightful", "elegant", "fascinating", "gorgeous", "heavenly", "irresistible", "joyful", "kind-hearted", "lovely", "mesmerizing", "nurturing", "outstanding", "passionate", "radiant", "sweet", "tender", "unforgettable", "vibrant", "warm-hearted", "exquisite", "youthful", "zealous"];
+const upliftingAdj = ["inspiring", "motivational", "encouraging", "empowering", "positive", "hopeful", "energetic", "optimistic", "cheerful", "happy", "joyful", "bright", "enthusiastic", "vibrant", "confident", "passionate", "dynamic", "exciting", "upbeat", "invigorating"];
+const naughtyAdj = ["sexy", "sultry", "seductive", "alluring", "enticing", "provocative", "hottie", "hot", "ravishing", "captivating", "charming", "tempting", "sensual", "irresistible", "naughty"];
