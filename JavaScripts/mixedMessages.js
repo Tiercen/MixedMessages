cba23ff32640b4b11bdb50e479f5f7cad901a8b8
Tiercen <tiercen@gmail.com>
2024-03-27 07:09:59 -0400

    Updated mixedMessages.js with several new  arrays

--- JavaScripts/mixedMessages.js
+++ JavaScripts/mixedMessages.js
@@ -2,6 +2,12 @@
 
 //Arrays
 
+//Adjectives
 const lovingAdj = ["adorable", "beautiful", "charming", "delightful", "elegant", "fascinating", "gorgeous", "heavenly", "irresistible", "joyful", "kind-hearted", "lovely", "mesmerizing", "nurturing", "outstanding", "passionate", "radiant", "sweet", "tender", "unforgettable", "vibrant", "warm-hearted", "exquisite", "youthful", "zealous"];
 const upliftingAdj = ["inspiring", "motivational", "encouraging", "empowering", "positive", "hopeful", "energetic", "optimistic", "cheerful", "happy", "joyful", "bright", "enthusiastic", "vibrant", "confident", "passionate", "dynamic", "exciting", "upbeat", "invigorating"];
 const naughtyAdj = ["sexy", "sultry", "seductive", "alluring", "enticing", "provocative", "hottie", "hot", "ravishing", "captivating", "charming", "tempting", "sensual", "irresistible", "naughty"];
+const affirmationAdj = ["amazing", "smart", "beautiful", "spectacular", "awesome", "strong", "capable", "brave", "loved", "enough", "talented", "worthy", "resilient", "inspiring", "confident", "determined", "courageous", "appreciated", "valued", "remarkable"];
+
+//Actions
+const sexualActions = ["caress", "touch", "embrace", "cuddle", "nuzzle", "lick", "bite", "kiss", "rub", "stroke", "fondle", "hug", "snuggle", "squeeze", "pet", "smooch", "suck"];
+const affirmationActions = ["change the world", "look good", "make a difference", "shine bright", "be confident", "stay positive", "inspire others", "be myself", "stay strong", "keep going"];
